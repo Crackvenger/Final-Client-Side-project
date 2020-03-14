579d936aa5217971a6ad50d2a46b518be09d817b
Crackvenger <tsukinome123@gmail.com>
2020-03-15 00:35:17 +0800

    Addedthe files

---
+++ script.js
@@ -0,0 +1,23 @@
+const canvas = document.getElementById("theCanvas");
+const ctx = canvas.getContext('2d');
+var img = document.getElementById('cards');
+
+const Cards = {
+    pic: img,
+    x: 100,
+    y: 600,
+    w: 720,
+    h: 300
+};
+
+function insertCards() {
+    ctx.drawImage(
+        Cards.pic, 
+        Cards.x, 
+        Cards.y, 
+        Cards.w, 
+        Cards.h);
+};
+
+insertCards();
+
