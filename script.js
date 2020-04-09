8278512fe4b5aa3d881364e7d2d22beffdab4a6d
Crackvenger <tsukinome123@gmail.com>
2020-04-09 09:23:04 +0800

    Trying to get this to work

--- script.js
+++ script.js
@@ -11,8 +11,6 @@ const Cards = {
     h: 300
 };
 
-dist()
-
 
 function insertCards() {
    
@@ -24,19 +22,32 @@ function insertCards() {
         Cards.h);
 };
 
-insertCards();
+function getMousePosition(object, event) { 
+    let rect = object.getBoundingClientRect(); 
+    let x = event.clientX - rect.left; 
+    let y = event.clientY - rect.top; 
+    console.log("Coordinate x: " + x,  
+                "Coordinate y: " + y); 
+    if(x == Cards.x + Cards.w && y == Cards.y + Cards.h) {
+        console.log("Hit");
+    }
+} 
 
 function moveCards(){
     Cards.y = 100;
 };
 
 function update() {
-     moveCards();
+     moveCards();0
 
      insertCards();
 
-     console.log("Is this shit even working?");
+     console.log("check");
 };
 
 
-image.onmouseclick() = update;
+canvas.addEventListener("mousedown", function(e) {
+    getMousePosition(canvas, e);
+});
+
+insertCards();
