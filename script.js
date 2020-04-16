9814e0bbcb1eb18d16b6c07219d52344c7db3bfc
Crackvenger <tsukinome123@gmail.com>
2020-04-16 15:41:35 +0800

    fialed

--- script.js
+++ script.js
@@ -6,48 +6,64 @@ var image = document.getElementById('cards');
 const Cards = {
     pic: image,
     x: 100,
-    y: 600,
-    w: 720,
-    h: 300
+    y: 650,
+    width: 720,
+    height: 250
 };
 
 
 function insertCards() {
-   
     ctx.drawImage(
         Cards.pic, 
         Cards.x, 
         Cards.y, 
-        Cards.w, 
-        Cards.h);
+        Cards.width, 
+        Cards.height);
 };
+
 
 function getMousePosition(object, event) { 
     let rect = object.getBoundingClientRect(); 
     let x = event.clientX - rect.left; 
     let y = event.clientY - rect.top; 
-    console.log("Coordinate x: " + x,  
-                "Coordinate y: " + y); 
-    if(x == Cards.x + Cards.w && y == Cards.y + Cards.h) {
-        console.log("Hit");
+    return{
+        x,y
     }
 } 
 
+function isInside(pos, rect){
+    return pos.x > rect.x && pos.x < rect.x+rect.width && pos.y < rect.y+rect.height && pos.y > rect.y;
+}
+
 function moveCards(){
-    Cards.y = 100;
+    Cards.y = 600;
 };
 
 function update() {
-     moveCards();0
+    ctx.clearRect(
+        Cards.x,
+        Cards.y,
+        Cards.width,
+        Cards.height
+        )
+
+     moveCards(); 
 
      insertCards();
 
      console.log("check");
 };
 
+insertCards();
 
 canvas.addEventListener("mousedown", function(e) {
-    getMousePosition(canvas, e);
-});
+    var pos = getMousePosition(canvas, e);
 
-insertCards();
+    if(isInside(pos,Cards)) {
+        update();
+    }
+    else {
+        console.log("not hit");
+    }
+}, false);
+
